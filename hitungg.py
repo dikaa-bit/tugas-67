f325049c3ad2c0fab4898eb273c4ba32c9913a7d
dikaa-bit <DIKARSKII90@GMAIL.COM>
2025-04-26 00:21:08 +0700

    Add files via upload

---
+++ hitungg.py
@@ -0,0 +1,9 @@
+def jumlah(angka1,angka2):
+    hasil = angka1 + angka2
+    print('Hasil penjumlahan :',hasil)
+    print('------------')
+
+def kali(angka1,angka2):
+    hasil = angka1 * angka2
+    print('Hasil perkalian :',hasil)
+    print('----------------------')
---
+++ mainrog.py
@@ -0,0 +1,6 @@
+import hitungg
+
+angka1 = int(input('Masukan angka 1 : '))
+angka2 = int(input('Masukan nagka 2 : '))
+
+hitungg.jumlah(angka1,angka2)
